fd7ffeddf4bc976d16158d782665d15830449d20
tronicdata <larrybuffaloboy@gmail.com>
2015-07-26 13:27:15 -0400

    got jquery ajax pulling my feed

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<?php
-	// init class
-	$instagram = new Instagram(array(
-        	'apiKey'      => 'a5144346058d4b7f879c73ac2f77fe13',
-        	'apiSecret'   => '7bcbc6763f7e40aeadc347c4d687184b',
-        	'apiCallback' => 'YOUR_APP_CALLBACK' //must point to success.php
-        ));
-	
-	// create login URL
-	$loginUrl = $instagram->getLoginUrl();
-
-?>
-
 <html>
 <head>
 	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
@@ -18,5 +5,8 @@
 </head>
 <body>
 	<h1>Hello, how'ya doin'</h1>
+	<div class="posts">
+
+	</div>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 console.log("im here");
-
+$.ajax({
+	type: "GET",
+	dataType: "jsonp",
+	cache: false,
+	url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=ACCESS-TOKEN",
+	success: function(data) {
+		console.log(data);
+		console.log(data.data.length);
+		for (i=0; i < data.data.length; i++){
+			$('.posts').append('<img src="' + data.data[i].images.standard_resolution.url + '"/>');
+		}
+	}
+})
